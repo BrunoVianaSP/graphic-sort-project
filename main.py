cc4d4d0fcb3270fc6e8f653a99b3fbb92249ce09
Bruno Barbosa <bruno.barbosa.ext@conductor.com.br>
2019-02-18 12:32:06 -0300

    add variable vector

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import simplegui
 import random
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 500
+HEIGHT = 500
 
 vec = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
 
 random.shuffle(vec)
+
+def random_color():
+    # colors = ["White", "Blue", "Green", "Yellow"]
+    colors = ["White"]
+    random.shuffle(colors)
+    return colors[0]
 
 def need_swap(array, cur_index, bef_index):
     return array[cur_index] > array[bef_index]
@@ -17,26 +23,23 @@ def swap(array, cur_index, bef_index):
     array[bef_index] = swap 
 
 ### Normal ###
-def insertion_sort(array):    
-    for marker in range(1, len(array)):                
-        for index in range(marker, 0, -1):                    
-            if need_swap(array, index, index - 1):
-                swap(array, array, index, index - 1)                        
-                return
-    
-def selection_sort(array):        
-    stop = False    
+def selection_sort(array):            
     for marker in range(0, len(array) - 1):                
-        small = array[marker]
-        if stop:
-            break                    
+        small = array[marker]                    
         for index in range(marker + 1, len(array)):            
             if small < array[index]:                
                 small = array[index]                
                 array[index] = array[marker]
                 array[marker] = small
-                stop = True
-        
+                return
+
+def insertion_sort(array):
+    for marker in range(1, len(array)):                
+        for index in range(marker, 0, -1):                    
+            if need_swap(array, index, index - 1):
+                swap(array, index, index - 1)                        
+                return
+    
 def shell_sort(array):    
     stop = False
     swap = 0
@@ -107,15 +110,17 @@ def shell_sort_by_step(array):
         
         
 # Handler to draw on canvas
-def draw(canvas):    
+def draw(canvas):
     i = 0.01
     j = 0
     while i < 0.99:
         canvas.draw_line([WIDTH * i, HEIGHT * 0.99], 
-                         [WIDTH * i, HEIGHT * vec[j]], 
-                         1, "White")                
+                            [WIDTH * i, HEIGHT * vec[j]], 
+                            1, random_color())                
         i += 0.01
-        j += 1  
+        j += 1 
+           
+ 
         
 ### Fast Sort ###
 def selection_tick():
@@ -145,7 +150,7 @@ def shell_tick_by_step():
 
 def stop_if_sorted(vec, ticker):
     if isSorted(vec):
-        print "Sorted"        
+        print "Sorted"      
         ticker.stop()
 
 def isSorted(array):    
@@ -193,7 +198,7 @@ frame.add_button("Shell Sort By One", run_shell_by_step, 200)
 
 frame.set_draw_handler(draw)
 
-selection_timer = simplegui.create_timer(40, selection_tick)
+selection_timer = simplegui.create_timer(10, selection_tick)
 insertion_timer = simplegui.create_timer(40, insertion_tick)
 shell_timer = simplegui.create_timer(40, shell_tick)
 
